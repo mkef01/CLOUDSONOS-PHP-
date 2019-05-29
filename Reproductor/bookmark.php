d74c801a4a4c131ede78bbe8e13a4145a6bc74ed
mkef01 <mkef01@gmail.com>
2019-05-28 18:17:11 -0600

    Update bookmark.php

--- Reproductor/bookmark.php
+++ Reproductor/bookmark.php
@@ -2,23 +2,39 @@
 session_start();
   $album = $_POST['alb'];
   $usu = $_SESSION['usuario'];
-// Get cURL resource
-$curl = curl_init();
-// Set some options - we are passing in a useragent too here
-curl_setopt_array($curl, [
-    CURLOPT_RETURNTRANSFER => 1,
-    CURLOPT_URL => "http://localhost:56131/api/reproductor/bookmark?album=$album&usuario=$usu"
-]);
-// Send the request & save response to $resp
-$resp = curl_exec($curl);
-// Close request to clear up some resources
-curl_close($curl);
+   $url = "http://localhost:56131/api/reproductor/bookmark";
 
-if($resp == 'true'){
-	//echo "<script>alert('Guardado a Favoritos')</script>";
+
+  $data2 = array(
+    'album' => $album,
+    'usuario' => $usu
+);
+$payload2 = json_encode($data2);
+
+// Prepare new cURL resource
+$ch2 = curl_init($url);
+curl_setopt($ch2, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch2, CURLINFO_HEADER_OUT, true);
+curl_setopt($ch2, CURLOPT_POST, true);
+curl_setopt($ch2, CURLOPT_POSTFIELDS, $payload2);
+
+// Set HTTP Header for POST request 
+curl_setopt($ch2, CURLOPT_HTTPHEADER, array(
+'Content-Type: application/json',
+'Content-Length: ' . strlen($payload2))
+);
+
+// Submit the POST request
+$result2 = curl_exec($ch2);
+//$playlist = json_decode($result2);
+// Close cURL session handle
+curl_close($ch2);
+
+if($result2 == 'true'){
+	echo "<script>alert('Guardado a Favoritos')</script>";
 	header("location: Album.php?param=true");
 }else{
-	//echo "<script>alert('Error no se pudoguardar el album')</script>";
+	echo "<script>alert('Error no se pudoguardar el album')</script>";
 	header("location: Album.php?param=false");
 	}
 
